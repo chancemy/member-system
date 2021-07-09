f1f98d72dcb930cf0a079b150345af381f595f67
chancemy <38711710+chancemy@users.noreply.github.com>
2021-07-09 11:16:41 +0800

    0709

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,8 @@
 
     <!-- Styles -->
     <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+
+    
     @yield('css')
 </head>
 <body>
@@ -103,6 +105,7 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" ></script>
     <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+    
     @yield('js')
 </body>
 </html>
